778f629647fdfaa50bd589646aef800798f38f38
Pratik <pratikmirajkar34@gmail.com>
2022-12-11 19:59:29 +0530

    modified

--- OnlineRaddiwala_Project/index.php
+++ OnlineRaddiwala_Project/index.php
@@ -1,4 +1,4 @@
-<? include_once('./index.html');?>
-<? include_once('./home.html');?>
-<? include_once('./pickup.html');?>
-<? include_once('./login.html');?>
+<?php include_once('./index.html');?>
+<?php include_once('./home.html');?>
+<?php include_once('./pickup.html');?>
+<?php include_once('./login.html');?>
